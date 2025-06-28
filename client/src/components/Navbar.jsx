6477f80c762237ccd9d4210308b349318d34804f
syed musab <syedmusab585@gmail.com>
2025-06-28 19:44:44 +0500

    original

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 x1:mx-32'>
         <img onClick={()=>navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer'/>
         <button onClick={()=>navigate('/admin')} className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">Admin Login
-            {/* <img  src={assets.arrow} className="w-3" alt="arrow" /> */}
+            <img  src={assets.arrow} className="w-3" alt="arrow" />
             </button>
       
     </div>
