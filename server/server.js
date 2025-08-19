e5c8e228547756609e31c7ae851fc573308c2d72
syed musab <syedmusab585@gmail.com>
2025-08-19 08:34:37 +0500

    created public folder

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ await connectDB();
 
 //middlewares
 app.use(cors({
-    origin: "https://blogit-frontend-chi.vercel.app",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
