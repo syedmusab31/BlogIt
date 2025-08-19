17d724392badfa91b80d29c0630e6d4e1a53c4bb
syed musab <syedmusab585@gmail.com>
2025-08-19 08:53:16 +0500

    changes for vercel

--- server/server.js
+++ server/server.js
@@ -4,8 +4,15 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
+import path from 'path';
+import serverless from "serverless-http";
+
 
 const app = express();  
+// Serve static files from the "public" directory
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //db connection
 await connectDB();
@@ -22,6 +29,7 @@ app.use(express.json());
 const PORT=process.env.PORT || 5000;
 
 //routes
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.get('/',(req,res)=> res.send("Hello World"));
 app.use('/api/admin', adminRouter);
 app.use('/api/blog',blogRouter)
@@ -31,9 +39,11 @@ app.use('/api/blog',blogRouter)
 //   console.error(err.stack);
 //   res.status(500).json({ message: 'Something went wrong!' });
 // });
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-});
-
-export default app;
+export const handler = serverless(app);
